3c90dc5167df2d432df84e1c7c65c8fbf2678007
Shanshan Chen <shanshanc.chen@gmail.com>
2019-07-24 11:48:46 +0800

    feat: card component with sample data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import sampleWeatherData from './private/weatherData';
+import CardList from './components/CardList';
+
+const SAMPLE_DATA = sampleWeatherData
 
 function App() {
+  const [data] = useState(SAMPLE_DATA);
+  
   return (
     <div className="App">
       <h2>Weather App</h2>
+      <CardList dataList={data.list}/>
     </div>
   );
 }
---
+++ src/components/CardList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({dataList}) => (
+  <div className="card-list">
+    {dataList.map(data => 
+        <Card
+          day_of_week={data.dt_txt}
+          minTemp={data.main.temp_min}
+          maxTemp={data.main.temp_max}
+          weatherImage={data.weather[0].icon}
+          weatherAlt={data.weather[0].main}
+        />
+      )}
+    
+  </div>
+)
+
+export default CardList;
